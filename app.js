58e9f74b8a4872ada0200c10d050aabd343afb0b
remyallen <remy.a@icloud.com>
2016-03-01 07:41:57 -0600

    image bug fix and connection.js brought back

--- app.js
+++ app.js
@@ -2,79 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var pg = require('pg');
-//var dataRoute = require('./routes/data');
+var dataRoute = require('./routes/data');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var connectionString = '';
 
-if(process.env.DATABASE_URL !== undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
-} else {
-    connectionString = 'postgres://localhost:5432/petfinder';
-}
 
-//app.use('/data', dataRoute);
-app.post('/data', function(req, res) {
-    //console.log(req);
-
-    var addData = {
-        pet_name: req.body.name.$t,
-        pet_image_url: req.body.media.photos.photo[2].$t,
-        pet_description: req.body.description.$t.substring(0,99)
-    };
-
-    pg.connect(connectionString, function(err, client, done) {
-
-        client.query('INSERT INTO animal (pet_name, pet_image_url, pet_description) VALUES ($1, $2, $3)',
-            [addData.pet_name, addData.pet_image_url, addData.pet_description],
-
-            function (err, result){
-                done();
-                if(err) {
-                    console.log('error inserting data', err);
-                    res.send(false);
-                } else {
-                    res.send(result);
-                }
-            });
-        //var query = client.query('SELECT * FROM petfinder');
-
-        // Stream results back one row at a time
-        //query.on('row', function(row) {
-        //    results.push(row);
-        //});
-        //
-        //// close connection
-        //query.on('end', function() {
-        //    done();
-        //    return res.json(results);
-        //});
-        //
-        //if(err) {
-        //    console.log(err);
-        //}
-    });
-});
-
-app.get('/data', function(req, res) {
-    var results = [];
-    pg.connect(connectionString, function(err, client, done) {
-        var query = client.query('SELECT * FROM animal;');
-
-        query.on('row', function(row) {
-            results.push(row);
-        });
-        query.on('end', function() {
-            client.end();
-            return res.json(results);
-        });
-        if(err) {
-            console.log(err);
-        }
-    });
-});
+app.use('/data', dataRoute);
 
 
 
--- routes/data.js
+++ routes/data.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-//var connect = require('../modules/connection');
+var connect = require('../modules/connection');
 
 
-router.post('/data', function(req, res) {
-    console.log(req);
+router.post('/', function(req, res) {
+    //console.log(req.body);
 
     var addData = {
         pet_name: req.body.name.$t,
@@ -15,7 +15,7 @@ router.post('/data', function(req, res) {
 
     pg.connect(connect, function(err, client, done) {
 
-        client.query('INSERT INTO petfinder (pet_name, pet_image_url, pet_description) VALUES ($1, $2, $3)',
+        client.query('INSERT INTO animal (pet_name, pet_image_url, pet_description) VALUES ($1, $2, $3)',
             [addData.pet_name, addData.pet_image_url, addData.pet_description],
 
         function (err, result){
@@ -46,5 +46,22 @@ router.post('/data', function(req, res) {
     });
 });
 
+router.get('/', function(req, res) {
+    var results = [];
+    pg.connect(connect, function(err, client, done) {
+        var query = client.query('SELECT * FROM animal;');
+
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+        if(err) {
+            console.log(err);
+        }
+    });
+});
 
 module.exports = router;
